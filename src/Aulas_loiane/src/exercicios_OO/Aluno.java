e4490f8772e27a69caf784e3b3944f7c4c003bcd
Antonio-pf <antoniopfelipe03@gmail.com>
2020-07-28 22:11:17 -0300

    Ajustando arrays

--- src/Aulas_loiane/src/exercicios_OO/Aluno.java
+++ src/Aulas_loiane/src/exercicios_OO/Aluno.java
@@ -4,8 +4,33 @@ public class Aluno {
 
     private String nome;
     private String matricula;
-    private String[] cursoMatriculado;
-    private int [] nota;
+    private String nomeCurso;
+    private String[] disciplinas;
+    private double[][] notas;
+
+    public Aluno() {
+        this.disciplinas = new String[3];
+        this.notas = new double[3][4];
+
+    }
+
+    public Aluno(String nome, String matricula, String nomeCurso) {
+
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nomeCurso = nomeCurso;
+        this.disciplinas = new String[3];
+        this.notas = new double[3][4];
+
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
 
     public String getNome() {
         return nome;
@@ -23,30 +48,54 @@ public class Aluno {
         this.matricula = matricula;
     }
 
-    public String[] getCursoMatriculado() {
-        return cursoMatriculado;
+    public String[] getDisciplinas() {
+        return disciplinas;
     }
 
-    public void setCursoMatriculado(String[] cursoMatriculado) {
-        this.cursoMatriculado = cursoMatriculado;
+    public void setDisciplinas(String[] disciplinas) {
+        this.disciplinas = disciplinas;
     }
 
-    public double aprovado() {
+    public void informacaoAluno() {
+
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Matrícula " + this.matricula);
+        System.out.println("Nome do curso: " + this.nomeCurso);
+
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Notas : " + disciplinas[i]);
+
+
+            for (int j = 0; j < notas[i].length; j++) {
+                System.out.println(notas[i][j] + " ");
+
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public boolean verificaAprovacao(int indice) {
+
+        if (media(indice) >= 7) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public double media(int indice) {
 
         double soma = 0;
 
-        for (int i = 0; i < nota.length; i++) {
-            soma += nota[i];
-
+        for (int i = 0; i < notas[indice].length; i++) {
+            soma += notas[indice][i];
         }
-        if (soma >= 7) {
-            System.out.println("Aprovado!");
-            return soma;
-
-        }else {
-            System.out.println("Reprovado");
-            return soma;
-        }
-       
+        double media = soma / 4;
+        return media;
     }
 }
