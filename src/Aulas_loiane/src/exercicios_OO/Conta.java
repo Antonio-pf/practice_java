f614d59dc8ea078d793b5639b5facd72dda509e8
Antonio-pf <antoniopfelipe03@gmail.com>
2020-07-28 22:10:11 -0300

    Corrigindo lógica

--- src/Aulas_loiane/src/exercicios_OO/Conta.java
+++ src/Aulas_loiane/src/exercicios_OO/Conta.java
@@ -5,32 +5,72 @@ public class Conta {
     private int numero;
     private double saldo;
     private boolean especial;
-    private double limite;
+    private double limiteEspecial;
 
 
     public double getSaldo() {
+
         return saldo;
     }
 
     public void setSaldo(double saldo) {
+
         this.saldo = saldo;
     }
 
-    public void saque(double valor) {
+    public double getLimiteEspecial() {
+
+
+        return limiteEspecial;
+    }
+
+    public void setLimiteEspecial(double limiteEspecial) {
+        this.limiteEspecial = limiteEspecial;
+    }
+
+    public boolean isEspecial() {
+        return this.especial;
+    }
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+
+    public boolean saque(double valor) {
 
         if (this.saldo >= valor) {
-            System.out.println("Saque pode ser efetuado");
-        } else {
-            System.out.println("Saque indisponível. \nSaldo $" + this.saldo + "\nSaque desejado $" +valor);
+            this.saldo -= valor;
+            return true;
 
+        }else{
+            if(especial){
+                double limite = this.limiteEspecial + this.saldo;
+            if(limite >= valor){
+                this.saldo -= valor;
+                return true;
 
+            }else {
+                return  false;
+            }
+            }else {
+                return false;
+
+            }
         }
     }
     public void deposita(double valor){
 
         this.saldo += valor;
-        System.out.println("$" + saldo);
+        System.out.println("$" + this.saldo);
 
+    }
+    public void consultarSaldo(){
+
+        System.out.println("Saldo atual: $" + this.saldo);
+    }
+
+    public boolean verificarUsoChequeEspecial(){
+        return saldo < 0;
     }
 
 
