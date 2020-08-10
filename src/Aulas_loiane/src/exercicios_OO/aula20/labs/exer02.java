ab9db91182cfb6afc9712e83c5f5c421f6906c10
Antonio-pf <antoniopfelipe03@gmail.com>
2020-08-10 15:08:48 -0300

    Menor e maior valor

--- src/Aulas_loiane/src/exercicios_OO/aula20/labs/exer02.java
+++ src/Aulas_loiane/src/exercicios_OO/aula20/labs/exer02.java
@@ -6,36 +6,55 @@ public class exer02 {
 
     public static void main(String[] args){
 
-        Random numbersRandom = new Random();
         float arrayNumbers [] [] = new float[10][10];
 
+        Random numbersRandom = new Random();
+
         for (int i = 0 ; i < arrayNumbers.length; i ++){
-            System.out.println("Matriz: " + (i));
-
             for (int j = 0; j < arrayNumbers[i].length ; j++){
-                arrayNumbers[i][j] = numbersRandom.nextInt(10);
+                arrayNumbers[i][j] = numbersRandom.nextInt(100 );
 
-                System.out.println(arrayNumbers[i][j] );
+            }
+        }
+        for (int i = 0 ; i < arrayNumbers.length; i ++){
+            for (int j = 0; j < arrayNumbers[i].length ; j++) {
 
+                System.out.print( +arrayNumbers[i][j] + " - ");
             }
             System.out.println();
         }
 
-        float menor  = 0;
-        float maior = 0;
+        float menorL5  = 100;
+        float maiorL5 = 0;
+        int linha5 = 5;
 
-        for (int i = 4 ; i < arrayNumbers.length; i ++){
-
-            for (int j = 0; j < arrayNumbers[i].length ; j++){
-                if(arrayNumbers[i][j] > maior){
-                    maior = arrayNumbers[i][j];
-                }else if (arrayNumbers [i][j] < menor){
-                    menor = arrayNumbers[i][j];
+        for (int i = 0   ; i < arrayNumbers[linha5].length; i ++){
+                if(arrayNumbers[linha5][i] > maiorL5){
+                    maiorL5 = arrayNumbers[linha5][i];
                 }
+                if (arrayNumbers [linha5][i] < menorL5){
+                    menorL5 = arrayNumbers[linha5][i];
+                }
+        }
+
+        System.out.println("Menor valor da linha 5: " + menorL5 );
+        System.out.println("Maior valor da linha 5: " + maiorL5);
+
+        float menorC7  = 100;
+        float maiorC7 = 0;
+        int col7 = 7;
+        for (int i = 0 ; i < arrayNumbers.length; i ++){
+
+            if(arrayNumbers[i][col7] > maiorC7){
+                maiorC7 = arrayNumbers[i][col7];
+            }
+            if (arrayNumbers [i][col7] < menorC7){
+                menorC7 = arrayNumbers[i][col7];
             }
         }
 
-        System.out.println("Menor: " + menor + " Maior: " + maior);
+        System.out.println("Menor valor da coluna 7: " + menorC7 );
+        System.out.println("Maior valor da coluna 7: " + maiorC7 );
 
     }
 }
