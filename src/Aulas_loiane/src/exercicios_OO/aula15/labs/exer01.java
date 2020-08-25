6b502bd1017241021ee0ebee737fd3921c49f8e6
Antonio-pf <antoniopfelipe03@gmail.com>
2020-08-25 00:14:05 -0300

    Maior número

--- src/Aulas_loiane/src/exercicios_OO/aula15/labs/exer01.java
+++ src/Aulas_loiane/src/exercicios_OO/aula15/labs/exer01.java
@@ -1,4 +1,26 @@
 package exercicios_OO.aula15.labs;
 
+import java.util.Scanner;
+
 public class exer01 {
+
+    public static void main(String [] arg){
+
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = 0;
+        int num2 = 0;
+
+        System.out.println("Digite dois números: ");
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+
+        if (num1 > num2){
+            System.out.println(num1);
+        }else{
+            System.out.println(num2);
+        }
+
+    }
+
 }
