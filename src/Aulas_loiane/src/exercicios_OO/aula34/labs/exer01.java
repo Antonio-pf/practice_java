c825d843cc13a460d296d5272624aa51ae654099
Antonio-pf <antoniopfelipe03@gmail.com>
2020-10-09 20:34:59 -0300

    Testando o exer

--- src/Aulas_loiane/src/exercicios_OO/aula34/labs/exer01.java
+++ src/Aulas_loiane/src/exercicios_OO/aula34/labs/exer01.java
@@ -1,4 +1,16 @@
 package exercicios_OO.aula34.labs;
 
 public class exer01 {
+
+    public static void main(String[] args){
+
+        Contador contador = new Contador();
+
+        Contador.incrementar();
+        Contador.retornaValor();
+        Contador.zeraContador();
+        Contador.retornaValor();
+
+
+    }
 }
