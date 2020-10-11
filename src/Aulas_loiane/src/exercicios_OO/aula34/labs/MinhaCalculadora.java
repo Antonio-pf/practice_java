69360b2fd7891239222cbba9394db8f7dab00549
Antonio-pf <antoniopfelipe03@gmail.com>
2020-10-10 22:03:35 -0300

    ADD método fatorial

--- src/Aulas_loiane/src/exercicios_OO/aula34/labs/MinhaCalculadora.java
+++ src/Aulas_loiane/src/exercicios_OO/aula34/labs/MinhaCalculadora.java
@@ -28,13 +28,42 @@ public class MinhaCalculadora {
         return result;
     }
 
-
     public static double divisao(double numA, double numB) {
 
         double result = 0;
 
         result = Math.round(numA / numB);
         return result;
+    }
+
+    public static double potencia(double numA, double numB){
+
+        double result = 0;
+
+        result = Math.pow(numA , numB);
+        return result;
+
+    }
+
+    public static int fatorial(int num){
+
+        if(num == 0){
+
+
+        }
+        int total = 1;
+
+        for (int i = num; i > 1; i -- ){
+
+            total *= num;
+            num--;
+
+        }
+
+        System.out.println("Fatorial = " + total);
+
+        return total;
+
     }
 
 }
