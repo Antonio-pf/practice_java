f9cd7c17b4a8b547391bf04195cd92fc45f2783b
Antonio-pf <antoniopfelipe03@gmail.com>
2020-11-06 13:40:37 -0300

    Criando classes

---
+++ src/Aulas_loiane/src/exercicios_OO/aula36/labs/Player.java
@@ -0,0 +1,59 @@
+package exercicios_OO.aula36.labs;
+
+public class Player {
+
+    private String name;
+    private int age;
+    private String gender;
+    private String heigth;
+    private String weigth;
+    private String position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(String heigth) {
+        this.heigth = heigth;
+    }
+
+    public String getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(String weigth) {
+        this.weigth = weigth;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
---
+++ src/Aulas_loiane/src/exercicios_OO/aula36/labs/Team.java
@@ -0,0 +1,32 @@
+package exercicios_OO.aula36.labs;
+
+public class Team {
+
+    private String name;
+    private String location;
+    private Player player;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
