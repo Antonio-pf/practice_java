7246b3f3339431831e1510f8b76886640f434db1
Antonio-pf <antoniopfelipe03@gmail.com>
2020-11-16 23:06:33 -0300

    Function add

--- src/Aulas_loiane/src/estruturaDeDados/Vetor.java
+++ src/Aulas_loiane/src/estruturaDeDados/Vetor.java
@@ -1,13 +1,44 @@
 package estruturaDeDados;
 
-public class aula3 {
+public class Vetor {
 
-    public static void main(String[] args){
+            private String[] elements;
+            private int scale;
 
-        //adicionando elemento no final do vetor
+            //qntd de elem que desejo para o vetor
+            public Vetor(int capacity){
 
-        
+                this.elements = new String[capacity];
+                this.scale = 0;
 
 
-    }
+        }
+
+      /*  public void add(String element){
+
+                for (int i = 0; i<this.elements.length; i ++){
+                    if(this.elements[i] == null){
+                        this.elements[i] = element;
+                        break;
+                    }
+                }
+        }*/
+
+        public void add(String element){
+
+            try {
+
+                this.elements[this.scale] = element;
+                this.scale++;
+
+            }catch (ArrayIndexOutOfBoundsException exception){
+
+                System.out.println("Tamanho máx de elementos");
+
+            }
+
+
+
+
+        }
 }
