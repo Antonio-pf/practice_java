76a0fef0af146f5a1696bd536cb3269062924f87
Antonio-pf <antoniopfelipe03@gmail.com>
2020-11-19 20:14:34 -0300

    Created metho - toString

--- src/Aulas_loiane/src/estruturaDeDados/Vetor.java
+++ src/Aulas_loiane/src/estruturaDeDados/Vetor.java
@@ -1,9 +1,11 @@
 package estruturaDeDados;
 
+import java.util.Arrays;
+
 public class Vetor {
 
             private String[] elements;
-            private int scale;
+            private int scale;//tamanho
 
             //qntd de elem que desejo para o vetor
             public Vetor(int capacity){
@@ -24,21 +26,57 @@ public class Vetor {
                 }
         }*/
 
-        public void add(String element){
+         /*public void add(String element) throws Exception{
 
-            try {
+          if (this.scale < this.elements.length){
 
                 this.elements[this.scale] = element;
                 this.scale++;
 
-            }catch (ArrayIndexOutOfBoundsException exception){
+            }else {
+              throw new Exception("Vetor já está cheio. Valor máx para elementos =" + this.scale);
+          }
+        }*/
 
-                System.out.println("Tamanho máx de elementos");
+    public boolean add(String element)  {
 
-            }
+        if (this.scale < this.elements.length) {
 
-
-
+            this.elements[this.scale] = element;
+            this.scale++;
+            return true;
 
         }
+
+        return false;
+
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("[");
+
+        for (int i = 0; i < this.size()-1; i ++){
+            s.append(this.elements[i]);
+            s.append(", ");
+        }
+
+        if (this.size() > 0){
+            s.append(this.elements[this.size()-1]);
+
+        }
+        s.append("]");
+            return s.toString();
+
+    }
+
+    public int size(){
+
+        return this.scale;
+
+    }
+
 }
